fcf86525e7fc7269d7c5be81f2818d35b964bff9
FrenchCurie <hdmsjbrtc5@privaterelay.appleid.com>
2026-06-12 22:51:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Service worker — Dose · Distance · Temps */
-const CACHE = 'dosecalc-v13';
+const CACHE = 'dosecalc-v14';
 
 /* Fichiers de l'application à mettre en cache à l'installation */
 const SHELL = [
